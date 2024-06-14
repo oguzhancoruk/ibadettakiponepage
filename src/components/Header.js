4c46b12c576c3b8a4c8789ed42468da5fac9c1bb
Oğuzhan Çoruk <coruk_61@hotmail.com>
2024-06-14 17:59:34 +0300

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './assets/images/1.png';
+import logo1 from './assets/images/image.png';
+
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo">Ibadet Takip</div>
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="Logo" />
+        <img className="logo1" src={logo1} alt="Logo" />
+      </div>
       <nav>
-        <ul>
+        <ul className="nav-links">
           <li><Link to="/">Ana Sayfa</Link></li>
           <li><Link to="/features">Özellikler</Link></li>
           <li><Link to="/contact">İletişim</Link></li>
-          <li><Link to="/privacy-policy">İletişim</Link></li>
+          <li><Link to="/privacy-policy">Gizlilik Politikası</Link></li>
         </ul>
       </nav>
     </header>
